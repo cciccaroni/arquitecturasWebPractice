37097179b3e728505c244bec621ec5fbf0d8a262
gustavo <torrecillagustavo@gmail.com>
2018-04-28 15:31:19 -0300

    handleando el click del boton y enviando un request con ajax

--- app/mod_chat/controllers.py
+++ app/mod_chat/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, render_template
+from flask import Blueprint, session, render_template, request
 from werkzeug.utils import redirect
 
 from app import mod_index
@@ -7,9 +7,13 @@ from app.mod_database import db
 
 mod_chat = Blueprint('chat', __name__)
 
-@mod_chat.route('/', methods=['GET'])
+@mod_chat.route('/', methods=['GET', 'POST'])
 def chat():
-    if session.get('user_id'):
-        return render_template("bootstrap_prueba/bootstrap.html", users=User.query.all())
+    if request.method == 'GET':
+        if session.get('user_id'):
+            return render_template("bootstrap_prueba/bootstrap.html", users=User.query.all())
+        else:
+            return redirect("auth/signin")
     else:
-        return redirect("auth/signin")
+        return 'ok'
+
