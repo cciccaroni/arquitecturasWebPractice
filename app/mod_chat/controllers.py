42f53d085be6c3677731a89ec92d5480043bacdf
Chris <christian.c@geopagos.com>
2018-04-28 18:32:16 -0300

    Se envía a la vista de chat el usuario con el que va a chatear

--- app/mod_chat/controllers.py
+++ app/mod_chat/controllers.py
@@ -2,16 +2,17 @@ from flask import Blueprint, request, session, render_template
 
 from app.appModel.models import Conversation, User
 
-mod_chat = Blueprint('chat', __name__, url_prefix= "/chat")
+mod_chat = Blueprint('chat', __name__, url_prefix="/chat")
+
 
 @mod_chat.route('/<user_id>', methods=['GET'])
 def chat(user_id):
     actual_user = User.query.filter(User.id == session['user_id']).first()
     adressee_user = User.query.filter(User.id == user_id).first()
-    if(not actual_user.conversations):
+    if not actual_user.conversations:
         conversation = Conversation()
         actual_user.addConversation(conversation)
         adressee_user.addConversation(conversation)
 
 
-    return render_template("chat/chat.html", room='gus genio')
+    return render_template("chat/chat.html", adressee=adressee_user)
