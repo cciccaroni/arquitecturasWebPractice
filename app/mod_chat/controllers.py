24defd1ae5e0d1a037c6dd72cac8a9c072ffe5ec
gustavo <torrecillagustavo@gmail.com>
2018-04-29 20:27:00 -0300

    persistiendo conversaciones correctamente

--- app/mod_chat/controllers.py
+++ app/mod_chat/controllers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, session, render_template
 
 from app.appModel.models import Conversation, User
+from app.mod_database import db
 
 mod_chat = Blueprint('chat', __name__, url_prefix="/chat")
 
@@ -9,10 +10,13 @@ mod_chat = Blueprint('chat', __name__, url_prefix="/chat")
 def chat(user_id):
     actual_user = User.query.filter(User.id == session['user_id']).first()
     adressee_user = User.query.filter(User.id == user_id).first()
-    if not actual_user.conversations:
-        conversation = Conversation()
-        actual_user.addConversation(conversation)
-        adressee_user.addConversation(conversation)
+    conversations = actual_user.conversations
+    users_have_conversation = len(filter(lambda conversation: actual_user in conversation.users and adressee_user in conversation.users, conversations)) > 0
+    if not users_have_conversation:
+        conversation = Conversation([actual_user, adressee_user])
+        db.session.add(conversation)
+        db.session.commit()
 
 
     return render_template("chat/chat.html", adressee=adressee_user, actualUser = actual_user)
+
