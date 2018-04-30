0c9efd38115cb5501d37533c6f981d5201b42d50
mbianchetti <bianchetti.ml@gmail.com>
2018-04-29 21:40:07 -0300

    Andano la comunicacion de estados y mensajes.

--- app/mod_chat/events.py
+++ app/mod_chat/events.py
@@ -8,21 +8,43 @@ from .. import socketio
 def joined(message):
     """Sent by clients when they enter a room.
     A status message is broadcast to all people in the room."""
-    room = session.get('user_id')
-    join_room(room)
-    # emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+    user_id = session.get('user_id', None)
+    if not user_id:
+      return
+    
+    # Join into my room
+    join_room(user_id)
+
+    # Broadcast of my new status to all users.
+    my_user = User.query.get(user_id)
+    status = {'msg': {'id': my_user.id, 'name': str(my_user.name), 'type':'STATUS', 'status': 'ENTERED'}}
+    users = User.query.all()
+    for user in users:
+        print (str(user_id), 'sending join status to', str(user.id))
+        emit('status', status, room=user.id)
 
 
 @socketio.on('textMessage', namespace='/chat')
-def textMessageSent(message, users):
+def textMessage(message, users):
     """Mensaje de texto enviado por el cliente a un usuario en particular
       Se envia un evento tanto al emisor como al destinatario
       (emisor updetea la ui mostrando el nuevo mensaje cada vez que recibe un evento, lo mismo el destinatario)
     """
     print(message, users)
+    status = {'msg': {'msg': message, 'type': 'MESSAGE'}}
     for user in users:
-      emit('message', {'msg': message}, room=user)
-      print ('Emitido:', message, user)
+        print ('Sending:', message, user)
+        # Broadcasteo con un status, porque sino me tengo 
+        # que meter en su sala para mandarle un msg.
+        emit('status', status, room=int(user))
 
-
-
+@socketio.on('left', namespace='/chat')
+def left(message):
+    """Sent by clients when they leave a room.
+    A status message is broadcast to all people in the room."""
+    user_id = session.get('user_id')
+    status = {'msg': {'id': user_id, 'status': 'LEFT'}}
+    users = User.query.all()
+    for user in users:
+        print (str(user_id), 'sending left status to', user.name)
+        emit('status', status, room=user.id)
