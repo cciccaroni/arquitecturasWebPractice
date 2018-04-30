ae0b82df2a0c0a2a60b54fdc35b63c299663063f
mbianchetti <bianchetti.ml@gmail.com>
2018-04-30 00:42:11 -0300

    Arreglo el mini error de len(filter) y

--- app/mod_chat/controllers.py
+++ app/mod_chat/controllers.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, session, render_template
+from werkzeug.utils import redirect
 
 from app.appModel.models import Conversation, User
 from app.mod_database import db
@@ -9,9 +10,12 @@ mod_chat = Blueprint('chat', __name__, url_prefix="/chat")
 @mod_chat.route('/<user_id>', methods=['GET'])
 def chat(user_id):
     actual_user = User.query.filter(User.id == session['user_id']).first()
+    if not actual_user:
+      return redirect("auth/signin")
+
     adressee_user = User.query.filter(User.id == user_id).first()
     conversations = actual_user.conversations
-    users_have_conversation = len(filter(lambda conversation: actual_user in conversation.users and adressee_user in conversation.users, conversations)) > 0
+    users_have_conversation = len(list(filter(lambda conversation: actual_user in conversation.users and adressee_user in conversation.users, conversations))) > 0
     if not users_have_conversation:
         conversation = Conversation([actual_user, adressee_user])
         db.session.add(conversation)
