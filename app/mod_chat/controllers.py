fa84cb0854beef7e68178257958423d146f34687
ivanpostolski <ivan.postolski@gmail.com>
2018-05-01 20:51:42 -0300

    chat controller usando la api

--- app/mod_chat/controllers.py
+++ app/mod_chat/controllers.py
@@ -2,6 +2,9 @@ from flask import Blueprint, session, render_template
 from werkzeug.utils import redirect
 
 from app.appModel.models import Conversation, User
+from app.mod_conversation import conversation_api, login_api
+from app.mod_conversation.conversation_api import conversation_manager
+from app.mod_conversation.login_api import login_manager
 from app.mod_database import db
 
 mod_chat = Blueprint('chat', __name__, url_prefix="/chat")
@@ -9,17 +12,13 @@ mod_chat = Blueprint('chat', __name__, url_prefix="/chat")
 
 @mod_chat.route('/<user_id>', methods=['GET'])
 def chat(user_id):
-    actual_user = User.query.filter(User.id == session['user_id']).first()
-    if not actual_user:
+    fromUser = session['user_id']
+    if not login_manager.isAuthorized(fromUser):
       return redirect("auth/signin")
 
-    adressee_user = User.query.filter(User.id == user_id).first()
-    conversations = actual_user.conversations
-    users_have_conversation = len(list(filter(lambda conversation: actual_user in conversation.users and adressee_user in conversation.users, conversations))) > 0
-    if not users_have_conversation:
-        conversation = Conversation([actual_user, adressee_user])
-        db.session.add(conversation)
-        db.session.commit()
+    toUser = user_id
 
-    return render_template("chat/chat.html", adressee=adressee_user, actual_user=actual_user)
+    conversation = conversation_manager.startConversation(fromUser, toUser)
+
+    return render_template("chat/chat.html", adressee=conversation.toUser, actual_user=conversation.fromUser,messages = conversation.messages)
 
