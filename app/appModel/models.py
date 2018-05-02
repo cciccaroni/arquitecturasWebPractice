8caf561819a1ee453188200f66130536fe8386fa
gustavo <torrecillagustavo@gmail.com>
2018-05-02 20:54:08 -0300

    create group and listing in contact list

--- app/appModel/models.py
+++ app/appModel/models.py
@@ -1,31 +1,38 @@
 from app.mod_database import db
 from sqlalchemy.sql import *
 
-
 # esta tabla es necesaria para las relaciones muchos a muchos
+user_group = db.Table('user_group',
+                      db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                      db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True)
+                      )
+
 user_conversation = db.Table('user_conversation',
                              db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-                             db.Column('conversation_id', db.Integer, db.ForeignKey('conversation.id'), primary_key=True)
-)
+                             db.Column('conversation_id', db.Integer, db.ForeignKey('conversation.id'),
+                                       primary_key=True)
+                             )
+
 
 # Define a User model
 class User(db.Model):
-
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128),  nullable=False)
+    name = db.Column(db.String(128), nullable=False)
     conversations = db.relationship('Conversation', secondary=user_conversation, lazy='subquery',
                                     back_populates='users')
-    # Identification Data: email & password
-    email = db.Column(db.String(128),  nullable=False, unique=True)
-    password = db.Column(db.String(192),  nullable=False)
+    groups = db.relationship('Group', secondary=user_group, lazy='subquery',
+                             back_populates='users')
 
+    # Identification Data: email & password
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(192), nullable=False)
 
     # New instance instantiation procedure
     def __init__(self, name, email, password):
-        self.name     = name
-        self.email    = email
+        self.name = name
+        self.email = email
         self.password = password
 
     def addConversation(self, conversation):
@@ -37,50 +44,37 @@ class User(db.Model):
     # Required for login
     def is_authenticated(self):
         return True
-    
+
     def is_active(self):
         return True
-    
+
     def is_anonymous(self):
         return False
 
     def get_id(self):
         return self.id
 
+
 class Conversation(db.Model):
     __tablename__ = 'conversation'
 
     id = db.Column(db.Integer, primary_key=True)
     messages = db.relationship('Message', lazy=True,
-                                 backref='conversation')
+                               backref='conversation')
     users = db.relationship('User', secondary=user_conversation, lazy='subquery',
                             back_populates='conversations')
+    group = db.relationship('Group', lazy=True,
+                            backref='conversation', uselist=False)
 
-    def __init__(self, users):
+    def __init__(self, users=[], group=None):
         self.users = users
-
-
-    def __repr__(self):
-        return '<Conversation %r>' % (self.name)
-
-
-class ConversationName(db.Model):
-    __tablename__ = 'conversation_name'
-
-    id = db.Column(db.Integer, primary_key=True)
-    conversation = db.Column(db.Integer, db.ForeignKey('conversation.id'))
-    name = db.Column(db.String(128), nullable=False)
-
-    def __init__(self,conversation,name):
-        self.conversation = conversation
-        self.name = name
+        self.group = group
 
     def __repr__(self):
-        return '<ConversationName %r>' % (self.name)
+        return '<Conversation %r>' % (self.id)
 
 
 class Message(db.Model):
-
     __tablename__ = 'message'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -88,14 +82,31 @@ class Message(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'))
 
-    def __init__(self,message,user_id,conversation_id):
+    def __init__(self, message, user_id, conversation_id):
         self.message = message
         self.user_id = user_id
         self.conversation_id = conversation_id
 
+    def __repr__(self):
+        return '<Message %r>' % (self.message)
+
+
+class Group(db.Model):
+    __tablename__ = 'group'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    users = db.relationship('User', secondary=user_group, lazy='subquery',
+                            back_populates='groups')
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'))
+
+    def __init__(self, name, users):
+        self.name = name
+        self.users = users
 
     def __repr__(self):
-        return '<Message %r>' % (self.name)
+        return '<Group %r>' % (self.name)
+
 
 
 
--- app/mod_list/controllers.py
+++ app/mod_list/controllers.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, session, render_template
 from werkzeug.utils import redirect
 
-from app.appModel.models import User
+from app import db
+from app.appModel.models import User, Group
 from flask_login import login_required
 
 mod_list = Blueprint('list', __name__)
@@ -13,7 +14,8 @@ def chat():
     actual_user = User.query.filter(User.id == session['user_id']).first()
     if actual_user:
         return render_template("contacts/list.html", 
-                                users=User.query.all(), 
-                                actual_user=actual_user, text="1")
+                                users=User.query.filter(User.id != actual_user.id).all(),
+                                actual_user=actual_user,
+                                groups=Group.query.filter(Group.users.any(User.id == actual_user.id)).all())
     
     return redirect("auth/signin")
