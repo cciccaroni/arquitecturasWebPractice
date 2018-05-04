07ad121a872d5c260da776e14f4eb1455307531a
unknown <pablofromer@gmail.com>
2018-05-04 18:41:37 -0300

    imagenes en chat.html

--- app/mod_chat/chatEvents.py
+++ app/mod_chat/chatEvents.py
@@ -26,7 +26,7 @@ def joined():
 
 # Se deberia iterar el toIds, que vienen todos los ids a los que hay que enviar el evento
 @socketio.on('textMessage', namespace='/chat')
-def textMessage(json):
+def textMessage(text, recipients, conversationId, loggedUserName):
     """Mensaje de texto enviado por el cliente a un usuario en particular
       Se envia un evento tanto al emisor como al destinatario
       (emisor updetea la ui mostrando el nuevo mensaje cada vez que recibe un evento, lo mismo el destinatario)
@@ -40,11 +40,10 @@ def textMessage(json):
         return
 
     #TODO: armar un chat_manager que tenga el conversation_manager adentro
-    conversation_manager.log_message(user_id,json['msg'],json['conversationId'])
+    conversation_manager.log_message(user_id, text, conversationId)
 
-    status = {'msg': json['msg'], 'from': json['fromName']}
-    # TODO: SACAR ESTE EVAL POR EL AMOR DE DIOS!
-    for recipient in eval(json['toIds']):
+    status = {'msg': text, 'from': loggedUserName}
+    for recipient in recipients:
         emit('uiTextMessage', status, room=recipient)
     print("mensaje enviado a los miembros del chat")
 
@@ -77,24 +76,13 @@ def end_recording(recipients, conversationId, loggedUserName):
     del session['wavename']
 
 
+@socketio.on('imageMessage', namespace='/chat')
+def imageMessage(image, recipients, conversationId, loggedUserName):
+    """Imagen enviada por el cliente a un usuario en particular
+      Se envia un evento tanto al emisor como al destinatario
+      (emisor updetea la ui mostrando el nuevo mensaje cada vez que recibe un evento, lo mismo el destinatario)
+    """
+    status = {'image': image, 'from': loggedUserName}
+    for recipient in recipients:
+        emit('image', status, room=recipient)
 
-
-
-#dejo comentado hasta que lo metamos posta
-# @socketio.on('imageMessage', namespace='/chat')
-# def imageMessage(message, users):
-#     """Imagen enviado por el cliente a un usuario en particular
-#       Se envia un evento tanto al emisor como al destinatario
-#       (emisor updetea la ui mostrando el nuevo mensaje cada vez que recibe un evento, lo mismo el destinatario)
-#     """
-#     user_id = session.get('user_id', None)
-#     if not user_id:
-#       return
-#
-#     my_user = User.query.get(user_id)
-#     if not my_user:
-#       return
-#
-#     status = {'msg': message, 'from': {'name': str(my_user.name), 'id': my_user.id}}
-#     for user in users:
-#         emit('image', status, room=int(user))
