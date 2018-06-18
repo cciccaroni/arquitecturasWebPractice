1275536f6535b0cbfb649191c9aa0411be4a1877
Chris <christian.c@geopagos.com>
2018-06-18 19:19:18 -0300

    se envía el nombre de la plataforma

--- app/mod_api/exporter.py
+++ app/mod_api/exporter.py
@@ -89,7 +89,7 @@ def exportUsers():
     result = []
     users = User.query.filter(User.platform_id == 1)
     for user in users:
-        result.append({'id': user.id, 'name': user.name})
+        result.append({'id': user.id, 'name': user.name, 'platform': app.config.appName})
     app.logger.debug('Returning users.\n {}'.format(result))
     return result
 
